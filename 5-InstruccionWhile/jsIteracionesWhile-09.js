b8fd4b2dff869a8cfcb8fb3c402cdbd39eb267b3
Agustin <aguscarcacha@gmail.com>
2020-07-30 12:13:20 -0300

    Clase del 30/07

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -2,20 +2,44 @@
 Al presionar el botón pedir  números  hasta que el usuario quiera,
 mostrar el número máximo y el número mínimo ingresado.*/
 function mostrar()
-{	// declarar variables
-	var banderaDelPrimero;
-	var numeroIngresado;
-	var numeroMaximo;
-	var numeroMinimo;
+{
 	var respuesta;
-	//iniciar variables
-	banderaDelPrimero="es el primero";
-	respuesta='si';
-	while(respuesta=="si")
-	{
+	var maximo;
+	var minimo;
+	var numero;
+	var primeraVez
+	
+	respuesta=true;
+	primeraVez=true
+
+	while (respuesta){
+		do {
+			numero=prompt("Ingrese un numero");
+			numero = parseInt(numero);
+
+		} while(isNaN(numero));
+
+		if (primeraVez){
+			primeraVez=false;
+
+			maximo = numero;
+			minimo = numero;
+		} else {
+			if (numero > maximo){
+				maximo = numero;
+			}
+			if (numero<minimo){
+				minimo=numero;
+			}
+		}
+
+		respuesta=confirm("Desea continuar?");
+
 		
-		respuesta=prompt("desea continuar?");
+
+
 	}
-	txtIdMaximo.value=numeroMaximo;
-	txtIdMinimmo.value=numeroMinimo;
+	txtIdMaximo.value=maximo
+	txtIdMinimo.value=minimo
+
 }//FIN DE LA FUNCIÓN
--- 8-TPs/12-jsRisingBTL.js
+++ 8-TPs/12-jsRisingBTL.js
@@ -12,7 +12,69 @@ function ComenzarIngreso ()
  	//definicion de variables
  	var edadIngresada;
  	var sexoIngresado;
- 	var estadoCivilIngresado;
+	var estadoCivilIngresado;
+	var sueldoBruto;
+	var legajo;
+	var nacionalidad;
+
+	//PUNTO A
+	edadIngresada = prompt ("Ingrese su edad entre 18 y 90 años.");
+	edadIngresada = parseInt(edadIngresada);
+
+	while (!(edadIngresada >=18 && edadIngresada<=90)){
+		edadIngresada = prompt ("Unicamente entre 18 y 90 años.");
+	}
+	
+	//PUNTO B
+	sexoIngresado = prompt ("Ingrese su sexo: m o f");
+
+	while (sexoIngresado!="m"&&sexoIngresado!="f"){
+		prompt ("INGRESE SU SEXO (m o f)");
+	}
+	
+	//PUNTO C
+	estadoCivilIngresado= prompt("Ingrese su estado civil: 1-Soltero, 2-Casado, 3-Divorciado, 4-Viudo.");
+	while (isNaN(estadoCivilIngresado)||estadoCivilIngresado>5||estadoCivilIngresado<1){
+		estadoCivilIngresado=prompt("Ingrese su estado BIEN su estado civil con 1, 2, 3 o 4.");
+	}
+	
+	//PUNTO D
+	sueldoBruto = prompt("Ingrese su suledo bruto mayor a 8000.");
+	sueldoBruto = parseInt(sueldoBruto);
+	while (isNaN(sueldoBruto)||sueldoBruto>8000){
+		sueldoBruto = prompt("Error! el sueldo bruto debe ser mayor a 8000.");
+		sueldoBruto = parseInt(sueldoBruto);
+	}
+
+	//PUNTO E
+	legajo=prompt("Ingrese su numero de legajo sin ceros a la izquierda.");
+	while (isNaN(legajo)||legajo<1000||legajo>9999){
+		legajo=prompt("Invalido! ingrese BIEN su legajo (numero de 4 cifras sin ceros a la izquierda");
+
+	}
+	//PUNTO F
+	nacionalidad=prompt("Ingrese su nacionalidad. (A-argentinos, E-extranjeros, N-nacionalizados");
+	while (nacionalidad !="A" && nacionalidad !="E" && nacionalidad !="N"){
+		nacionalidad = prompt("Error!, ingrese su nacionalidad con A, para argentinos, E, para extranjeros o N para nacionalizados.");
+		
+	}
+
+	txtIdEdad.value=("Su edad es: ")+edadIngresada;
+	txtIdSexo.value=("Su sexo es: ")+sexoIngresado;
+	txtIdEstadoCivil.value=("Su estado civil es: ")+estadoCivilIngresado;
+	txtIdSueldo.value=("Su sueldo bruto es: ")+sueldoBruto;
+	txtIdLegajo.value=("Su legajo es: ")+legajo;
+	txtIdNacionalidad.value=("Su nacionalidad es: ")+nacionalidad;
+
+
+
+
+
+
+
+
+
+	 
 
 
 
