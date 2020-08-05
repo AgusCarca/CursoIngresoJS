9ec1d9a2248be81b0d1b10e6327fed83cabae240
Agustin <aguscarcacha@gmail.com>
2020-08-05 12:21:48 -0300

    Clase 5/08

--- 9-Parciales/2020_1Cuatri_recu_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_recu_fuentes/dos.js
@@ -1,4 +1,85 @@
+//Realizar el algoritmo que permita ingresar los datos de una compra de productos de la construccion, hasta que el cliente quiera:
+//Tipo validad("arena";"cal";"cemento")
+//Cantidad de bolsas,
+//Precio por bolsa (más de cero ),
+//al terminar la compra el cliente accede a un descuento segun las bolsas en total
+//Si compro más de 10 bolsas en total tenes 15% de descuento sobre el total a pagar.
+//Si compro más de 30 bolsas en total tenes 25% de descuento sobre el total a pagar.
+//a) El importe total a pagar , bruto sin descuento y...
+//b) el importe total a pagar con descuento(solo si corresponde)
+//d) Informar el tipo ("arena";"cal";"cemento") con mas cantidad de bolsas en el total de la compra.
+//f) El tipo mas caro
+
 function mostrar()
 {
-  alert("dos");
+  var item;
+  var cantidad;
+  var respuesta;
+  var cantidadBolsas=0;
+  var tipoBolsaMaxima;
+  var precioBolsa;
+  var acumuladorPrecioBruto=0;
+  var acumuladorCantidadBolsa=0;
+  var descuento=1;
+  var precioConDescuento;
+  var tipoDeBolsas;
+  var precioMaximo=0;
+
+  
+  
+  do {
+    do{
+    item=prompt("Ingrese item");
+    } while(!(item=="arena"||item=="cal"||item=="cemento")){
+      item=("Reingrese el item");
+
+    }
+    do{
+    cantidad=prompt("Ingrese cantidad");
+    } while(isNaN(cantidad)){
+      cantidad=prompt("Reingrese la cantidad");
+    }
+    do{
+    precioBolsa=prompt("Ingrese precio");
+    precioBolsa=parseInt(precioBolsa);
+    } while(isNaN(precioBolsa)||precioBolsa<0){
+      precioBolsa=prompt("Reingrese el precio");
+    }
+
+        acumuladorPrecioBruto += precioBolsa * cantidad;
+
+    acumuladorCantidadBolsa += cantidad;
+
+    respuesta=confirm("Desea continuar?");
+
+
+
+
+  }while(respuesta);
+
+  //A
+
+  console.log ("El precio bruto es: "+acumuladorPrecioBruto);
+
+  //B
+  if(acumuladorCantidadBolsa>10 && acumuladorCantidadBolsa<30){
+    descuento = 0,15;
+  } else if (acumuladorCantidadBolsa>30){
+    descuento = 0.25;
+  }
+
+    precioConDescuento = acumuladorPrecioBruto * descuento;
+  console.log ("El precio con descuento es: "+precioConDescuento);
+
+  //D
+  console.log("El tipo de bolsa maxima es: "+tipoDeBolsas);
+
+  //E
+  if(precioBolsa > precioMaximo){
+    precioMaximo
+  }
+
+
+
+    
 }
--- 9-Parciales/2020_1Cuatri_recu_fuentes/uno.js
+++ 9-Parciales/2020_1Cuatri_recu_fuentes/uno.js
@@ -1,5 +1,96 @@
 
 function mostrar()
 {
-	alert("uno");
+
+	var item;
+	var cantidad;
+	var fabricante;
+	var flagJabon = 0;
+	var precioMayorJabon;
+	var precioMayorJabonCantidad;
+	var cantidadItemMasUnidades;
+	var precioMayorJabonFabricante;
+	var promedioMasUnidades;
+	var cantidadTotalBarbijo = 0;
+	var contadorGeneral = 0;
+	var acumuladorCantidadesAlcohol=0;
+	var acumuladorCantidadesJabon=0;
+	var acumuladorCantidadesBarbijo=0;
+
+
+	for (i = 0; i < 5; i++) {
+
+		//ENTRADA
+		item=prompt("Por favor ingrese item");
+		while (!(item == "barbijo" || item == "jabon" || item == "alcohol")){
+			item=prompt("Por favor reingrese item");
+		}
+
+		cantidad=parseInt(prompt("Por favor ingrese cantidad"));
+		while (isNaN(cantidad) || cantidad < 0 && cantidad > 1000){
+			cantidad=parseInt(prompt("Por favor reingrese cantidad"));
+		}
+
+		precio=parseInt(prompt("Por favor ingrese precio"));
+		while (isNaN(precio)|| precio < 100 && precio > 300){
+			precio=prompt("Por favor reingrese precio");
+		}
+
+		fabricante=prompt("Por favor ingrese fabricante");
+		while (!(isNaN(fabricante))){
+			fabricante=prompt("Por favor reingrese fabricante ");
+		}
+		//-------------------------------------------------------------------------------
+		//a) Del más caro de los Barbijos, la cantidad de unidades y el fabricante
+		if (item == "jabon" && flagJabon == 0 || precio > precioMayorJabon){
+			precioMayorJabon =  precio;
+			precioMayorJabonCantidad = cantidad;
+			precioMayorJabonFabricante = fabricante;
+			flagJabon = 1;
+		}
+		
+		//b) Del tipo de producto con mas unidades en total de la compra, el promedio por compra
+		switch(item){
+			case "alcohol":
+				acumuladorCantidadesAlcohol+=cantidadItemMasUnidades;
+				break;
+			case "barbijo":
+				acumuladorCantidadesBarbijo+=cantidadItemMasUnidades;
+				break;
+			case "jabon":
+				acumuladorCantidadesJabon+=cantidadItemMasUnidades;
+				break;
+		}
+		if (acumuladorCantidadesAlcohol>acumuladorCantidadesBarbijo
+			&& acumuladorCantidadesAlcohol >acumuladorCantidadesJabon){
+				promedioMasUnidades=acumuladorCantidadesAlcohol;
+
+			}else if (acumuladorCantidadesBarbijo>acumuladorCantidadesJabon) {
+				promedioMasUnidades=acumuladorCantidadesBarbijo;
+
+			}else{
+				promedioMasUnidades=acumuladorCantidadesJabon;
+
+			}
+
+		
+
+
+		//c) Cuántas unidades de jabones hay en total
+		if (item == "barbijo"){
+			cantidadTotalBarbijo = cantidadTotalBarbijo + cantidad;
+		}
+		contadorGeneral++;
+	}//fin del while
+
+	//Mostrar por pantalla
+	document.write
+	(
+	"a) Del más caro de los jabones, la cantidad de unidades y el fabricante: "+"<br>"+
+	"Cantidad de Unidades: "+precioMayorJabonCantidad+"<br>"+
+	"Nombre de fabricante: "+precioMayorJabonFabricante+"<br>"+
+	"b) Del ítem con más unidades, el promedio: "+promedioMasUnidades+"<br>" +
+	"c) Cuántas unidades de barbijo hay en total: "+cantidadTotalBarbijo
+	)
+	
 }
